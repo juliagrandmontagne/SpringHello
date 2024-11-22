5eed2d0bc27dd281676b0f820faf9425862471e6
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-11-22 12:35:42 +0100

    commit

--- src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloControleur {
     @GetMapping
     public String direHello(){
-        return "Hello";
+        return "Helloyou";
     }
 }
